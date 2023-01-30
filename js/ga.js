52679fb14bfc6e457d0c6f66b90b77bde7940397
dx2331lxz <3434055686@qq.com>
2023-01-30 11:48:19 +0800

    Site updated: 2023-01-30 11:48:19

---
+++ js/ga.js
@@ -0,0 +1,9 @@
+<script>
+  window.ga_tid = 'Your google_site_id';
+  (function() {
+    var dga = document.createElement("script");
+    dga.src = "https://rmt.dogedoge.com/fetch/public/ga.js";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(dga, s);
+  })();
+</script>
---
+++ js/perload.js
@@ -0,0 +1,9 @@
+<script>
+  window.FPConfig = {
+    delay: 0,    // 浏览器空闲多少秒后开始预加载
+    ignoreKeywords: [],    // 不进行预加载的链接，例 ["#", "/about"]
+    maxRPS: 3,    // 每秒最大加载数
+    hoverDelay: 50    // 鼠标悬浮后预加的延迟，毫秒单位
+    // 上方数值未默认值
+  };
+</script>
